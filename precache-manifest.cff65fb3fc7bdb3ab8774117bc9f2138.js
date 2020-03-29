5d4333d5da96fdbccf793db4fed426edc63acac7
terara <a.terarara@gmail.com>
2020-03-29 18:55:49 +0900

    Updates

---
+++ precache-manifest.cff65fb3fc7bdb3ab8774117bc9f2138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6462f63341026c0a99e273d283a670ea",
+    "url": "/fanatag-app/index.html"
+  },
+  {
+    "revision": "84aa421281068c9549df",
+    "url": "/fanatag-app/static/css/main.9f0dbf7c.chunk.css"
+  },
+  {
+    "revision": "766950050b340803b88c",
+    "url": "/fanatag-app/static/js/2.63120664.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fanatag-app/static/js/2.63120664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84aa421281068c9549df",
+    "url": "/fanatag-app/static/js/main.7b84894c.chunk.js"
+  },
+  {
+    "revision": "b9e7aac922c4bebfa691",
+    "url": "/fanatag-app/static/js/runtime-main.bd30e6b7.js"
+  }
+]);
